146d5539223ae5c7b55543c6001f2f251da3ce83
Benjamin <sabean.ben@gmail.com>
2018-01-22 16:20:58 -0400

    Added documentation

--- DataBase/DataEntry.py
+++ DataBase/DataEntry.py
@@ -1,11 +1,18 @@
 import MySQLdb
 
-class DataEntry:  
+# This calss acts as a wrapper for the MySQLdb class.
+# TYpical use includes inserting new data into the databse.
+class DataEntry:
 
+    # Initialization routine.
     def __init__(self, myDb, myCursor):
         self.c = myCursor
         self.db = myDb
-        
+
+    # Creates a query to insert data into the database.
+    # Param table The table to insert data into.
+    # Param arg The data to insert into the table.
+    # Return The query as a string.
     def createInsertQuery(self, table, arg):
         try:
             query = "INSERT INTO `" + table + "` VALUES ("
@@ -17,19 +24,27 @@ class DataEntry:
             pass
 
         return query
-    
-    def close(self):
-        self.db.close() 
 
+    # Close the connection to the database.
+    def close(self):
+        self.db.close()
+
+    # Get the contents of the selected table.
+    # Param table The table whose contents will be found.
+    # Return the contents of the table as a double indexed list.
     def getTable(self, table):
         self.c.execute("SELECT * FROM " + table)
         return self.c.fetchall()
-    
+
+    # Insert a line of data into the database.
+    # Param table The table to insert data into.
+    # Param data The data to insert into tha database
+    # Return True if the data is successfully inserted, False otherwise.
     def insertData(self, table, data):
         arg = []
         for n in data:
             arg.append(n)
-            
+
         try:
             print("Creating query")
             self.c.execute(self.createInsertQuery(table,arg))
--- DataBase/DbManager.py
+++ DataBase/DbManager.py
@@ -1,18 +1,24 @@
 import MySQLdb
 from DataEntry import DataEntry
 
+# Class to create, insert and manage tables in a database. Acts as a wrapper
+# for the DataEntry class adding aditional functionality to retrieve number of
+# sensors and sample rate for DAQ Modules.
 class DbManager:
     def __init__(self, myHost, myUser, myPasswd, myDb):
 
+        # Connect to MySQL batabase.
         self.data=MySQLdb.connect(host=myHost,user=myUser,
         passwd=myPasswd,db=myDb)
 
         self.c=self.data.cursor()
         self.mydb = myDb
         self.db = DataEntry(self.data, self.c)
-        self.deviceTable = "Devices"
+        self.deviceTable = "Devices"     # Table of all DAQ modules
         self.addressTable = "Addresses"
 
+    # Find all tables for DAQ modules.
+    # Returns list of tables.
     def __getTables(self):
         try:
             self.c.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES "
@@ -22,6 +28,10 @@ class DbManager:
         except:
             pass
 
+    # Find device name from device ID.
+    # Param deciceID The ID given to the DAQ module as defined by
+    #       the `Device ID` column in the Devices table.
+    # Returns the name of the device.
     def __decodeID(self, deviceID):
         try:
             self.c.execute("SELECT `Table Name` FROM `" + self.deviceTable + "` WHERE "
@@ -30,6 +40,8 @@ class DbManager:
         except:
             pass
 
+    # Find all registered devices.
+    # Returns a list of all registered devices.
     def __getDevices(self):
         try:
             self.c.execute("SELECT `Table Name` FROM `" + self.deviceTable + "`")
@@ -37,6 +49,9 @@ class DbManager:
         except:
             pass
 
+    # Find the number of sensors that is supposed to be attached to a device.
+    # Param DeviceID The ID given to the DAQ module.
+    # Returns The number of sensors registered to a device.
     def getSensorCount(self, DeviceID):
         try:
             self.c.execute("SELECT Sensors FROM `" + self.deviceTable + "` WHERE "
@@ -45,12 +60,22 @@ class DbManager:
         except:
             pass
 
+    # Find if a table exists for the geven DAQ module.
+    # Param table A lists of all tables for DAQ modules.
+    # Param device The mane of the device to see whether a table
+    #      already exists for it.
+    # Returns True if the table already exists, False otherwise.
     def __tableExists(self,tables,device):
         if(any(device in x for x in tables)):
             return True
         else:
             return False
 
+    # Create a new table for the given DAQ module
+    # Param tablename The device's name to be used as a table name.
+    # Param numSensors The number of sensors registered to the DAQ module
+    #     Sensors will be named in the format: Sensor1, Sensor2, ... , Sensor N
+    # Returns True if the table was created successfully, False otherwise.
     def __createTable(self,tableName, numSensors):
 
         query = "CREATE TABLE `" + tableName + "` (`TimeStamp` timestamp, "
@@ -59,13 +84,20 @@ class DbManager:
             query += "`Sensor" + `(i+1)` + "` double,"
         query = query[:-1]
         query += ');'
-        
+
         try:
             self.c.execute(query)
             return True
         except:
             return False
 
+    # Insert a new entry into the database.
+    # Param DeviceID The ID given to the DAQ module.
+    # Param numSensors The number of sensors registered to the device.
+    # Param data the data to be inserted into the database. Inserted data
+    #      should be of the form ["NULL" (or timestamp), Sensor1 data,
+    #      sensor2 data, ..., SensorN data]
+    # Return True if the data was successfully inserted, false otherwise.
     def insertData(self, deviceID, numSensors, data):
         table = self.__decodeID(deviceID)
         if(not self.__tableExists(self.__getTables(), table)):
@@ -77,6 +109,10 @@ class DbManager:
         else:
             return False
 
+    # Find the sample rate for a given DAQ module.
+    # Param deviceID The ID given to the DAQ module.
+    # Return The sample rate of the device as defined in
+    #      `Time Interval (Min)` column of the Devices table.
     def getSampleRate(self, deviceID):
         try:
             self.c.execute("SELECT `Time Interval (Min)` FROM `" + self.deviceTable + "` WHERE "
@@ -85,6 +121,10 @@ class DbManager:
         except:
             pass
 
+    # Find the number of Devices registered in the system. This 
+    #      function ignores the Eagle Eye as it behaves differently
+    #      from all other DAQ modules.
+    # Return The number of devices registered in the system.
     def getDeviceCount(self):
         try:
              self.c.execute("SELECT COUNT(*) FROM `" + self.deviceTable + "` WHERE `Device ID` > 0")
